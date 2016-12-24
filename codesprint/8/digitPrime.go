25a9f444ac98d72c253109f725be55d176659dc5
Gary Capell <gary.capell@gmail.com>
2016-12-24 12:33:29 +1100

    Much faster, but wrong :-(

--- codesprint/8/digitPrime.go
+++ codesprint/8/digitPrime.go
@@ -3,196 +3,153 @@ package main
 import (
 	"fmt"
 	"log"
-	"sort"
+	"time"
 )
 
+const (
+	modulus   = 1e9 + 7
+	maxDigits = 400e3
+	debug     = false
+)
+
+var totals [maxDigits]int
+
 func main() {
-	m, initialStateNums := prep()
-	expCache[1] = m
+	findTotals()
 
 	var q int
 	fmt.Scan(&q)
-	ns := make([]int, q)
 	for j := 0; j < q; j++ {
-		fmt.Scan(&ns[j])
+		var n int
+		fmt.Scan(&n)
+		fmt.Println(totals[n])
 	}
-	sort.Ints(ns)
-
-	for ns[0] < 5 {
-		ns = ns[1:]
-	}
-	prev := ns[0]
-	m = exp(prev - 4)
-	fmt.Println(m.sumCols(initialStateNums))
-
-	for _, n := range ns {
-		diff := n - prev
-		if diff > 0 {
-			diffM := exp(diff)
-			m = mul(m, diffM)
-		}
-		fmt.Println(m.sumCols(initialStateNums))
-		prev = n
-	}
-	fmt.Println("multiplies: ", multiplies, " cacheHits: ", cacheHits)
 }
 
-type state struct{ a, b, c, d int }
-
-func prep() (matrix, []int) {
+func findTotals() {
 	prime := make([]bool, 46)
 	for _, p := range []int{2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43} {
 		prime[p] = true
 	}
-	transitions := make(map[state][]state)
-	var initialStates []state
-	stateNum := make(map[state]int)
-	nextStateNum := 0
+	var prime4 []state // 4-tuples that are 3-sum-prime and 4-sum-prime
 	for a := 0; a <= 9; a++ {
 		for b := 0; b <= 9; b++ {
 			for c := 0; c <= 9; c++ {
+				if !prime[a+b+c] {
+					continue
+				}
 				for d := 0; d <= 9; d++ {
-					if prime[a+b+c+d] && prime[a+b+c] && prime[b+c+d] {
-						s := state{a, b, c, d}
-						transitions[s] = nil
-						stateNum[s] = nextStateNum
-						nextStateNum++
-						if a > 0 {
-							initialStates = append(initialStates, s)
-						}
+					if prime[a+b+c+d] && prime[b+c+d] {
+						prime4 = append(prime4, state{a, b, c, d})
 					}
 				}
 			}
 		}
 	}
 
-	for s := range transitions {
+	transitions := make(map[state][]state) // dst -> []src
+	for _, s := range prime4 {
 		for e := 0; e <= 9; e++ {
-			if prime[s.c+s.d+e] &&
-				prime[s.b+s.c+s.d+e] &&
-				prime[s.a+s.b+s.c+s.d+e] {
-				dest := state{s.b, s.c, s.d, e}
-				transitions[s] = append(transitions[s], dest)
+			p := prime
+			if p[s.c+s.d+e] && p[s.b+s.c+s.d+e] && p[s.a+s.b+s.c+s.d+e] {
+				dst := state{s.b, s.c, s.d, e}
+				transitions[dst] = append(transitions[dst], s)
 			}
 		}
 	}
-	matrixSize = len(transitions)
-	m := newMatrix()
-	for src, dsts := range transitions {
-		for _, dst := range dsts {
-			m.set(stateNum[dst], stateNum[src], 1)
+
+	stateNum := sequencer(make(map[state]int))
+	for dst, srcs := range transitions {
+		stateNum.add(dst)
+		for _, s := range srcs {
+			stateNum.add(s)
 		}
 	}
-	var initialStateNums []int
-	for _, s := range initialStates {
-		initialStateNums = append(initialStateNums, stateNum[s])
-	}
-	return m, initialStateNums
-}
 
-var expCache = make(map[int]matrix)
-var cacheHits int
-
-func exp(n int) matrix {
-	if n < 1 {
-		log.Fatal("n", n)
-	}
-	if m, ok := expCache[n]; ok {
-		cacheHits++
-		return m
-	}
-	p := largestPowerOf2LTE(n)
-
-	if p == n {
-		m2 := exp(n / 2)
-		m := mul(m2, m2)
-		expCache[n] = m
-		return m
-	}
-	m := mul(exp(p), exp(n-p))
-	if n < 1024 {
-		expCache[n] = m
-	}
-	return m
-}
-
-func largestPowerOf2LTE(n int) int {
-	j := 1
-	for j <= n {
-		j *= 2
-	}
-	return j / 2
-}
-
-var matrixSize int
-
-const modulus = 1e9 + 7
-
-type matrix struct {
-	rows, cols sparseVector
-}
-
-type sparseVector []map[int]int
-
-func newMatrix() matrix {
-	return matrix{
-		rows: newSparseVector(),
-		cols: newSparseVector(),
-	}
-}
-
-func newSparseVector() sparseVector {
-	return make([]map[int]int, matrixSize)
-}
-
-func (m matrix) set(r, c, val int) {
-	m.rows.set(r, c, val)
-	m.cols.set(c, r, val)
-}
-
-func (v sparseVector) set(r, c, val int) {
-	if v[r] == nil {
-		v[r] = make(map[int]int)
-	}
-	v[r][c] = val
-}
-
-var multiplies int
-
-func mul(a, b matrix) matrix {
-	multiplies++
-	reply := newMatrix()
-
-	for r := 0; r < matrixSize; r++ {
-		for c := 0; c < matrixSize; c++ {
-			v := sparseDotProduct(a.rows[r], b.cols[c])
-			if v != 0 {
-				reply.set(r, c, v%modulus)
-			}
+	var m1 [][2]int
+	var m2 [][3]int
+	var m3 [][4]int
+	var m4 [][5]int
+	var m5 [][6]int
+	sn := stateNum
+	for dst, srcs := range transitions {
+		switch len(srcs) {
+		case 1:
+			m1 = append(m1, [2]int{sn[dst], sn[srcs[0]]})
+		case 2:
+			m2 = append(m2, [3]int{sn[dst], sn[srcs[0]], sn[srcs[1]]})
+		case 3:
+			m3 = append(m3, [4]int{sn[dst], sn[srcs[0]], sn[srcs[1]], sn[srcs[2]]})
+		case 4:
+			m4 = append(m4, [5]int{sn[dst], sn[srcs[0]], sn[srcs[1]], sn[srcs[2]], sn[srcs[3]]})
+		case 5:
+			m5 = append(m5, [6]int{sn[dst], sn[srcs[0]], sn[srcs[1]], sn[srcs[2]], sn[srcs[3]], sn[srcs[4]]})
+		default:
+			log.Fatal("too many", len(srcs))
 		}
 	}
-	return reply
-}
 
-func sparseDotProduct(a, b map[int]int) int {
-	if a == nil || b == nil {
-		return 0
-	}
-	var reply int
-	for k, v := range a {
-		if v2, ok := b[k]; ok {
-			reply += v * v2
+	s1 := make([]int, len(stateNum))
+	for s, n := range stateNum {
+		if s.a != 0 {
+			s1[n] = 1
 		}
 	}
-	return reply
+	s2 := make([]int, len(stateNum))
+
+	totals[1] = 9 // Cheated and copied these.
+	totals[2] = 90
+	totals[3] = 303
+	totals[4] = 280
+
+	start := time.Now()
+	for k := 5; k < len(totals); k++ {
+		total := 0
+		if debug {
+			fmt.Println(s1)
+		}
+		for _, m := range m1 {
+			s2[m[0]] = s1[m[1]]
+			total += s1[m[1]]
+		}
+		for _, m := range m2 {
+			i := s1[m[1]] + s1[m[2]]
+			s2[m[0]] = i % modulus
+			total += i
+		}
+		for _, m := range m3 {
+			i := s1[m[1]] + s1[m[2]] + s1[m[3]]
+			s2[m[0]] = i % modulus
+			total += i
+		}
+		for _, m := range m4 {
+			i := s1[m[1]] + s1[m[2]] + s1[m[3]] + s1[m[4]]
+			s2[m[0]] = i % modulus
+			total += i
+		}
+		for _, m := range m5 {
+			i := s1[m[1]] + s1[m[2]] + s1[m[3]] + s1[m[4]] + s1[m[5]]
+			s2[m[0]] = i % modulus
+			total += i
+		}
+		totals[k] = total % modulus
+		s1, s2 = s2, s1
+	}
+	if debug {
+		fmt.Printf("%d in %v\n", len(totals), time.Since(start))
+	}
 }
 
-func (m matrix) sumCols(cols []int) int {
-	reply := 0
-	for _, c := range cols {
-		for _, v := range m.cols[c] {
-			reply += v
-		}
+type state struct{ a, b, c, d int }
+
+func (s state) String() string {
+	return fmt.Sprintf("%d%d%d%d", s.a, s.b, s.c, s.d)
+}
+
+type sequencer map[state]int
+
+func (m sequencer) add(s state) {
+	if _, ok := m[s]; !ok {
+		m[s] = len(m)
 	}
-	return reply % modulus
 }
